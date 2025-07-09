1bb004f1de61e37ab94b21322feaedf78ee7b187
Akshaj Katkuri <akshaj2312@gmail.com>
2025-07-09 13:14:29 -0400

    added negative numbers support

--- pseudo.py
+++ pseudo.py
@@ -8,7 +8,7 @@ class Position:
         self.fn = fn
         self.ftxt = ftxt
 
-    def advance(self, current_char):
+    def advance(self, current_char=None):
         self.idx += 1
         self.col += 1
 
@@ -39,18 +39,19 @@ class IllegalCharError(Error):
         super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
 class InvalidSyntaxError(Error): 
-    def __init__(self, pos_start, pos_end, error_name, details=''):
+    def __init__(self, pos_start, pos_end, details=''):
         super().__init__(pos_start, pos_end, 'Invalid Synstax', details)
 
 # Token types
-TYPE_INT = 'TYPE_INT'
-TYPE_FLOAT = 'TYPE_FLOAT'
-TYPE_PLUS = 'TYPE_PLUS'
-TYPE_MINUS = 'TYPE_MINUS'
-TYPE_MUL = 'TYPE_MUL'
-TYPE_DIV = 'TYPE_DIV'
-TYPE_LPAREN = 'TYPE_LPAREN'
-TYPE_RPAREN = 'TYPE_RPAREN'
+TYPE_INT = 'INT'
+TYPE_FLOAT = 'FLOAT'
+TYPE_PLUS = 'PLUS'
+TYPE_MINUS = 'MINUS'
+TYPE_MUL = 'MUL'
+TYPE_DIV = 'DIV'
+TYPE_LPAREN = 'LPAREN'
+TYPE_RPAREN = 'RPAREN'
+TYPE_EOF = 'EOF'
 
 class Token(): 
     def __init__(self, type_, value=None, pos_start:Position=None, pos_end:Position=None): 
@@ -88,22 +89,22 @@ class Lexer:
             if self.current_char in ' \t': 
                 self.advance()
             elif self.current_char == '+': 
-                tokens.append(Token(TYPE_PLUS))
+                tokens.append(Token(TYPE_PLUS, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '-': 
-                tokens.append(Token(TYPE_MINUS))
+                tokens.append(Token(TYPE_MINUS, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '*':
-                tokens.append(Token(TYPE_MUL))
+                tokens.append(Token(TYPE_MUL, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '/':
-                tokens.append(Token(TYPE_DIV))
+                tokens.append(Token(TYPE_DIV, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '(':
-                tokens.append(Token(TYPE_LPAREN))
+                tokens.append(Token(TYPE_LPAREN, pos_start=self.pos))
                 self.advance()
             elif self.current_char == ')':
-                tokens.append(Token(TYPE_RPAREN))
+                tokens.append(Token(TYPE_RPAREN, pos_start=self.pos))
                 self.advance()
             elif self.current_char.isdigit(): 
                 tokens.append(self.make_number())
@@ -113,11 +114,13 @@ class Lexer:
                 self.advance()
                 return [], IllegalCharError(pos_start, self.pos.idx, f"'{char}'")
 
+        tokens.append(Token(TYPE_EOF, pos_start=self.pos))
         return tokens, None
 
     def make_number(self): 
         num_str = ''
         dot_count = 0 # to check if float or integer
+        pos_start = self.pos.copy()
 
         while self.current_char != None and (self.current_char.isdigit() or self.current_char == '.'):
             if self.current_char == '.':
@@ -129,9 +132,9 @@ class Lexer:
             self.advance()
             
         if dot_count == 0: 
-            return Token(TYPE_INT, int(num_str))
+            return Token(TYPE_INT, int(num_str), pos_start=pos_start, pos_end=self.pos)
         else: 
-            return Token(TYPE_FLOAT, float(num_str))
+            return Token(TYPE_FLOAT, float(num_str), pos_start=pos_start, pos_end=self.pos)
         
 # Nodes for PEMDAS operations tree
 
@@ -150,22 +153,34 @@ class BinaryOperatorNode:
 
     def __repr__(self):
         return f'({self.left_node}, {self.operator_token}, {self.right_node})'
+    
+class UnaryOperatorNode: 
+    def __init__(self, operator_token, node):
+        self.operator_token = operator_token
+        self.node = node
+
+    def __repr__(self):
+        return f'({self.operator_token}, {self.node})'
 
 class ParseResult: 
     def __init__(self):
         self.error = None
         self.node = None
 
-    def register(eslf): 
-        if isinstance(res, ParseResult): 
-            if res.error: self.error = res.error
-            return res.node
+    def register(self, response): 
+        if isinstance(response, ParseResult): 
+            if response.error: self.error = response.error
+            return response.node
+        
+        return response
 
-    def success(self): 
-        pass
+    def success(self, node): 
+        self.node = node
+        return self
 
-    def failure(self): 
-        pass
+    def failure(self, error): 
+        self.error = error
+        return self
 
 # Parser
 
@@ -182,15 +197,40 @@ class Parser:
         return self.current_token
     
     def parse(self): 
-        res = self.expr()
-        return res
+        result = self.expr()
+        if not result.error and self.current_token.type != TYPE_EOF:
+            return result.failure(InvalidSyntaxError(
+                self.current_token.pos_start, self.current_token.pos_end, "Expected '+', '-', '*', or '/'"
+            ))
+        return result
     
     def factor(self): 
-        tok = self.current_token
+        response = ParseResult()
+        token = self.current_token
 
-        if tok.type in (TYPE_INT, TYPE_FLOAT): 
-            self.advance()
-            return NumberNode(tok)
+        if token.type in (TYPE_PLUS, TYPE_MINUS): 
+            response.register(self.advance())
+            factor = response.register(self.factor())
+            if response.error: return response
+            return response.success(UnaryOperatorNode(token, factor))
+        
+        elif token.type in (TYPE_INT, TYPE_FLOAT): 
+            response.register(self.advance())
+            return response.success(NumberNode(token))
+        
+        elif token.type == TYPE_LPAREN: 
+            response.register(self.advance())
+            expr = response.register(self.expr())
+            if response.error: return response
+            if self.current_token.type == TYPE_RPAREN: 
+                response.register(self.advance())
+                return response.success(expr)
+            else:
+                return response.failure(InvalidSyntaxError(
+                    self.current_token.pos_start, self.current_token.pos_end, "Expected ')'"
+                ))
+        
+        return response.failure(InvalidSyntaxError(token.pos_start, token.pos_end, "Expected int or float"))
 
     def term(self): 
         return self.binary_operation(self.factor, (TYPE_DIV, TYPE_MUL))
@@ -199,15 +239,18 @@ class Parser:
         return self.binary_operation(self.term, (TYPE_PLUS, TYPE_MINUS))
     
     def binary_operation(self, func, op_tokens): 
-        left = func()
+        result = ParseResult()
+        left = result.register(func())
+        if result.error: return result
 
         while self.current_token.type in op_tokens: 
             op_token = self.current_token
-            self.advance()
-            right = func()
+            result.register(self.advance())
+            right = result.register(func())
+            if result.error: return result
             left = BinaryOperatorNode(left, op_token, right)
 
-        return left
+        return result.success(left)
 
 def run(fn, text):
     lexer = Lexer(fn, text)
@@ -218,4 +261,4 @@ def run(fn, text):
     parser = Parser(tokens)
     tree = parser.parse()
 
-    return tree, None
+    return tree.node, tree.error
