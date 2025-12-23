947b7a76ae70b85ad0a52bf39d8cf36c0d8465ce
Akshaj Katkuri <akshaj2312@gmail.com>
2025-12-22 19:11:02 -0500

    fixed main.py for how it is supposed to be

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from interpreter import run
-from robot.robot import Robot
-
-robot = Robot()
 
 while True: 
     text = input('>>>')
     if text.strip() == "": continue
-    # Pass the Robot instance into the interpreter so built-ins can use it
-    result, error = run('<stdn>', text, robot=robot)
+    result, error = run('<stdn>',text)
 
     if error: 
         print(error.as_string())
